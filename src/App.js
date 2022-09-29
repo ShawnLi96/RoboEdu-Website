f315f64406eb74031a66f0c99232d9739c7d210d
Orion Chen <orion.chen@hotmail.com>
2022-09-29 13:47:02 -0400

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      hello 
+      <Header name="orion" age={17}/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <h1> *** 2022 Summer Camp *** </h1>
+                <h2>线下夏令营</h2>
+            </div>
+        );
+        
+    }
+    
+
+}
