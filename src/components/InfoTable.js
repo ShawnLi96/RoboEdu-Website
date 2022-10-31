eaefe35aea79511278669bf5f4196f692775c755
Orion <orion.chen@hotmail.com>
2022-10-30 23:37:48 -0400

    .

--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -1,129 +1,121 @@
-import React, { useMemo } from 'react'
-import styled from 'styled-components'
-import { useTable, useSortBy } from 'react-table'
-import { COLUMNS } from '../data/columns'
-import MOCK_DATA from '../MOCK_DATA.json'
-import '../css/table.css'
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import { useTable, useSortBy } from "react-table";
+import { COLUMNS } from "../data/columns";
+import MOCK_DATA from "../MOCK_DATA.json";
+import "../css/table.css";
 
+import check from "../images/check.png";
+import cross from "../images/cross.png";
 
-import check from '../images/check.png'
-import cross from '../images/cross.png'
+export default function InfoTable() {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
 
-
-export default function InfoTable(){
-
-    const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => MOCK_DATA, [])
-
-    var formdata = new FormData();
-    
-    formdata.append("authkey", "f1a4e3f7e24acd3d2af968f44b856adacbb5d9951118b360de4323339f7abe521ade6f721e44ede2a3bb5986ffb114bf");
-    formdata.append("studentid", 3);
-
-
-  
-
-    fetch(`http://localhost:160/students/getstudent`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({studentid: 3, authkey: "f1a4e3f7e24acd3d2af968f44b856adacbb5d9951118b360de4323339f7abe521ade6f721e44ede2a3bb5986ffb114bf"})
-    }).then(res => {
-        console.log(res)
-        return res.json();
-    }).then((res) => console.log(res))
-
-    
-      
-    const tableInstance = useTable({
-        columns,
-        data
+  fetch(`http://localhost:160/parents/getorders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      parentid: 22,
+      authkey:
+        "f99b3e0accc55b4e8df73e83e430590257dc03a4f6ac859608773d0952a04acee359c7dfeced23be88fac3a7f160e836",
+    }),
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json();
     })
-    
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = tableInstance
+    .then((res) => console.log(res));
 
-    var formatter = new Intl.NumberFormat('en-CA', {
-        style: 'currency',
-        currency: 'CAD'
-    });
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
 
-    for (let i = 0; i < data.length; i++){
-        let total = 0;
-        if (data[i].aftercare) total += 50;
-        if (data[i].beforecare) total += 50;
-        if (data[i].lunch) total += 50;
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
 
-        data[i].subtotal = formatter.format(total);
-    }
+  var formatter = new Intl.NumberFormat("en-CA", {
+    style: "currency",
+    currency: "CAD",
+  });
 
-    return (
-        <Container>
-            <table {...getTableProps()}>
-                <thead>
-                    {
-                        headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {
-                                headerGroup.headers.map((column) => (
-                                    <th {...column.getHeaderProps()}> {column.render('Header')} </th>
-                                ))
-                            }
-                            
-                        </tr>
-        
+  // for (let i = 0; i < data.length; i++){
+  //     let total = 0;
+  //     if (data[i].aftercare) total += 50;
+  //     if (data[i].beforecare) total += 50;
+  //     if (data[i].lunch) total += 50;
 
-                        ))
-                    }
+  //     data[i].subtotal = formatter.format(total);
+  // }
 
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {
-                        rows.map((row) => {
-                            prepareRow(row)
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {
-                                        row.cells.map((cell) => {
-                                            return <td {...cell.getCellProps()}> {
-                                                (typeof cell.value === "boolean") ? <Icon state={cell.value}/> : cell.render('Cell')}
-                                            </td>
-                                        })
-
-                                    }
-                                </tr>
-                            )
-                        })
-
-                    }
-                </tbody>
-
-            </table>
-        </Container>
-    );   
+  return (
+    <Container>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {" "}
+                  {column.render("Header")}{" "}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {
+            // for each order by parent ID
+            // for each student by  order ID
+            // fetch camperlist info by student ID
+            rows.map((order) => {
+              prepareRow(order);
+              return (
+                <tr {...order.getRowProps()}>
+                  {order.map((student) => {
+                    return (
+                      <tr {...student.getRowProps()}>
+                        {student.cells.map((cell) => {
+                          return (
+                            <td {...cell.getCellProps()}>
+                              {" "}
+                              {typeof cell.value === "boolean" ? (
+                                <Icon state={cell.value} />
+                              ) : (
+                                cell.render("Cell")
+                              )}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 5vh;
-`
+  display: flex;
+  justify-content: center;
+  margin-top: 5vh;
+`;
 
 const Icon = styled.div`
-    width: 15px;
-    height: 15px;
-    background-repeat: no-repeat;
-    background-image: url(${(props) => props.state ? check : cross});
-    background-size: cover;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 5px;    
-    
-
-`
+  width: 15px;
+  height: 15px;
+  background-repeat: no-repeat;
+  background-image: url(${(props) => (props.state ? check : cross)});
+  background-size: cover;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px;
+`;
