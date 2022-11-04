6a7108b989aea7075df50b8ebaa3f6b9a94518e6
Orion <orion.chen@hotmail.com>
2022-11-04 16:37:18 -0400

    trying to fix table

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,8 @@
 import React, { useState, useEffect } from "react";
 import InfoTable from "./NewInfoTable";
-import { fetchCamper, fetchStudent, fetchOrders } from "../data/fetch";
-import styled from 'styled-components'
+import { fetchOrders } from "../data/fetch";
 
-import check from "../images/check.png";
-import cross from "../images/cross.png";
 export default function Home() {
-  var formatter = new Intl.NumberFormat("en-CA", {
-    style: "currency",
-    currency: "CAD",
-  });
-  const weeks = [
-    "",
-    "July 1st to 7th",
-    "July 8th to 14th",
-    "July 15th to 21st",
-    "July 22nd to 29th",
-    "August 1st to 7th",
-    "August 8th to 14th",
-    "August 15th to 21st",
-    "August 22nd to 29th",
-  ];
 
       
   const parentid = 22
@@ -34,6 +16,8 @@ export default function Home() {
       return res;
       });
       setOrders(await orders);
+      console.log('ORDERS')
+      console.log(orders)
   };
 
   useEffect(() => {
@@ -41,75 +25,10 @@ export default function Home() {
       getOrders();
     }, []);
   
-  // a 2D array, where each 1D array corresponds to each order
-  // allCamperInfo[i][x] stores a HTML row element for a camper
-  // displayed on row x of order i
-  const [allCamperInfo, setAllCamperInfo] = useState([]);
-
-  const getCampers = async () => {
-    const masterCamperInfo = [];
-    allOrders.map((order) => {
-      const orderCamperData = [];
-      const campers = JSON.parse(order["CamperIDs"]);
-      campers.map(async (camper) => {
-        const camperData = await fetchCamper(camper).then((res) => {
-          return res;
-        });
-
-        // needed to fetch name, as campers/getCamper does not provide name
-        const student = await fetchStudent(camperData["Student ID"]).then(
-          (res) => {
-            return res;
-          }
-        );
-
-        // create a name key pair in the camperData
-        camperData["Name"] = student["first name"] + " " + student["last name"];
-
-        // create an entry for the camper for this order in the table
-        return orderCamperData.push(
-          <tr key={`camper-${camper}`}>
-            <td>{weeks[camperData["Week"]]}</td>
-            <td>{camperData["Name"]}</td>
-            <td>{camperData["Program ID"]}</td>
-            <td><Icon state = {camperData["Lunch"] === 1}/> </td>
-            <td><Icon state = {camperData["BeforeExt"] === 1}/> </td>
-            <td><Icon state = {camperData["AfterExt"] === 1}/> </td>
-            <td>
-              {formatter.format(
-                camperData["Lunch"] * 50 +
-                  camperData["BeforeExt"] * 50 +
-                  camperData["AfterExt"] * 5
-              )}
-            </td>
-          </tr>
-        );
-      });
-
-      return masterCamperInfo.push(orderCamperData);
-    });
-
-    setAllCamperInfo(masterCamperInfo);
-  };
-
-  useEffect(() => {
-    console.log("second render");
-    getCampers();
-  }, [allOrders]);
 
   return (
     <>
-      <InfoTable data={allCamperInfo}></InfoTable>
+      <InfoTable orders={allOrders}></InfoTable>
     </>
   );
 }
-const Icon = styled.div`
-  width: 15px;
-  height: 15px;
-  background-repeat: no-repeat;
-  background-image: url(${(props) => (props.state ? check : cross)});
-  background-size: cover;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 5px;
-`
--- src/components/NewInfoTable.js
+++ src/components/NewInfoTable.js
@@ -1,8 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { COLUMNS } from "../data/columns";
 import "../css/table.css";
+import { fetchCamper, fetchStudent } from "../data/fetch";
+import styled from 'styled-components'
+
+import check from "../images/check.png";
+import cross from "../images/cross.png";
 
 export default function NewInfoTable(props) {
+    // a 2D array, where each 1D array corresponds to each order
+  // allCamperInfo[i][x] stores a HTML row element for a camper
+  // displayed on row x of order i
+  const [allCamperInfo, setAllCamperInfo] = useState([]);
+
+  const getCampers = async () => {
+    const masterCamperInfo = [];
+    props.orders.map((order) => {
+      const orderCamperData = [];
+      const campers = JSON.parse(order["CamperIDs"]);
+      campers.map(async (camper) => {
+        const camperData = await fetchCamper(camper).then((res) => {
+          return res;
+        });
+
+        // needed to fetch name, as campers/getCamper does not provide name
+        const student = await fetchStudent(camperData["Student ID"]).then(
+          (res) => {
+            return res;
+          }
+        );
+
+        // create a name key pair in the camperData
+        camperData["Name"] = student["first name"] + " " + student["last name"];
+
+        // create an entry for the camper for this order in the table
+        return orderCamperData.push(
+          <tr key={`camper-${camper}`}>
+            <td>{weeks[camperData["Week"]]}</td>
+            <td>{camperData["Name"]}</td>
+            <td>{camperData["Program ID"]}</td>
+            <td><Icon state = {camperData["Lunch"] === 1}/> </td>
+            <td><Icon state = {camperData["BeforeExt"] === 1}/> </td>
+            <td><Icon state = {camperData["AfterExt"] === 1}/> </td>
+            <td>
+              {formatter.format(
+                camperData["Lunch"] * 50 +
+                  camperData["BeforeExt"] * 50 +
+                  camperData["AfterExt"] * 5
+              )}
+            </td>
+          </tr>
+        );
+      });
+
+      return masterCamperInfo.push(orderCamperData);
+    });
+
+    setAllCamperInfo(masterCamperInfo);
+  };
+
+  useEffect(() => {
+    console.log("second render");
+    getCampers();
+  }, [props.orders]);
+
+
   return (
     <>
       <table>
@@ -14,11 +76,11 @@ export default function NewInfoTable(props) {
           </tr>
         </thead>
         <tbody>
-          {props.data.map((order) => {
+          {allCamperInfo.map((order) => {
             return (
               <>
+                {console.log(order)}
                 {order}
-
                 <tr>
                   <td colSpan={7}>Summary</td>
                 </tr>
@@ -30,3 +92,28 @@ export default function NewInfoTable(props) {
     </>
   );
 }
+const Icon = styled.div`
+  width: 15px;
+  height: 15px;
+  background-repeat: no-repeat;
+  background-image: url(${(props) => (props.state ? check : cross)});
+  background-size: cover;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px;
+`
+var formatter = new Intl.NumberFormat("en-CA", {
+  style: "currency",
+  currency: "CAD",
+});
+const weeks = [
+  "",
+  "July 1st to 7th",
+  "July 8th to 14th",
+  "July 15th to 21st",
+  "July 22nd to 29th",
+  "August 1st to 7th",
+  "August 8th to 14th",
+  "August 15th to 21st",
+  "August 22nd to 29th",
+];
