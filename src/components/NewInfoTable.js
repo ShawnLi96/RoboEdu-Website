2b6ddc687af328674267af972cf7bf3a2e1ad547
Orion <orion.chen@hotmail.com>
2022-11-27 20:43:52 -0500

    merge

--- src/components/NewInfoTable.js
+++ src/components/NewInfoTable.js
@@ -18,20 +18,22 @@ export default function NewInfoTable(props) {
           </thead>
           {
             props.data.map((order, i) => {
-              var total = 0;
-              return (
-                <tbody key={i}>
-                  {order.map((entry) => {
-                    // maps thru every camper of the particular order
-                    // returns it as a <tr> HTML element
-                    total += entry[1];
-                    return entry[0];
-                  })}
-                  <tr>
-                    <td colSpan={7}>Summary: {formatter.format(total)}</td>
-                  </tr>
-                </tbody>
-              );
+              if (order.length > 0){
+                var total = 0;
+                return (
+                  <tbody key={i}>
+                    {order.map((entry) => {
+                      // maps thru every camper of the particular order
+                      // returns it as a <tr> HTML element
+                      total += entry[1];
+                      return entry[0];
+                    })}
+                    <tr>
+                      <td colSpan={7}>Summary: {formatter.format(total)}</td>
+                    </tr>
+                  </tbody>
+                );
+              }
             }) 
             
           }
