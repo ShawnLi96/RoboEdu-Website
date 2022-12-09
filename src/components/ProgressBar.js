cd43305c806f96db051a6e23d7f9739c8d83c442
Orion Chen <orion.chen@hotmail.com>
2022-12-09 14:10:01 -0500

    .

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,12 +1,28 @@
 import React from "react";
 import styled from 'styled-components'
 import { devices } from "../../data/devices"
+import title1 from "../../images/title1.png"
+import title2 from "../../images/title2.png"
+import title3 from "../../images/title3.png"
+import title4 from "../../images/title4.png"
+import title5 from "../../images/title5.png"
+
 
 
 export default function ProgressBar(props) {
+    const titles = [title1, title2, title3, title4, title5];
 
     return(
-        <>
-        </>
+        <Container>
+            <Arrow number = {0}></Arrow>
+
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+`
+const Arrow = styled.div`
+    background-image: ${(props) => titles[props.number]}
+`
